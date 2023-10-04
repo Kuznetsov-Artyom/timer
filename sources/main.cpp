7c9da53c000e1313958e5ac5c6eb084a8321302c
Artyom <artyomka_working@mail.ru>
2023-10-04 15:46:40 +0300

    add timer.hpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "timer.hpp"
+
 int main() {
-  std::cout << "Hello World!\n";
+  TIMER_START(timer, tmr::nanosecond_t);
+
+  for (int i = 0; i < 10'000'000; ++i) {
+  }
+  std::cout << TIMER_GET(timer) << '\n';
+
   return 0;
 }
